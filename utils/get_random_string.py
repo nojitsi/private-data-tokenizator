804c910629de9846425ad4520409a0ba2611852a
Yehor Iliushchenko <buaueru@gmail.com>
2023-04-06 20:55:09 +0300

    Random string now use only capital letters

--- utils/get_random_string.py
+++ utils/get_random_string.py
@@ -2,4 +2,5 @@ import random
 import string
 
 def get_random_string(length):
-    return ''.join(random.choice(string.ascii_letters) for i in range(length))
+    return ''.join(random.choice(string.ascii_uppercase) for i in range(length))
+
